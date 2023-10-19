46b8dfc6df98ff43df0e35ed8b23bc292df16abe
15rtrujillo <ryantrujillo@comcast.net>
2023-10-18 22:28:03 -0600

    Now runs in about 700ms. Nice :)

--- Smooshed Morse Code 2/main.cpp
+++ Smooshed Morse Code 2/main.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <future>
 #include <iostream>
-#include <map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "node.hpp"
@@ -14,7 +14,7 @@
 std::string morseCode =
 ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. "
 "... - ..- ...- .-- -..- -.-- --..";
-std::map<char, std::string> lettersToCode;
+std::unordered_map<std::string, char> codeToLetters;
 
 void populateMap()
 {
@@ -26,18 +26,10 @@ void populateMap()
 		std::string code =
 			morseCode.substr(lastSpaceIndex, spaceIndex - lastSpaceIndex);
 		lastSpaceIndex = spaceIndex + 1;
-		lettersToCode.insert(std::pair<char, std::string>(letter++, code));
+		codeToLetters.insert(std::pair<std::string, char>(code, letter++));
 	}
-	/*
-	for (auto iter = lettersToCode.begin(); iter != lettersToCode.end(); ++iter)
-	{
-		std::cout << iter->first << ": " << iter->second << std::endl;
-	}
-	*/
 }
 
-// std::unique_ptr<Node> consumeNextCode(std::string& morse, int currentIndex, std::vector<char>& letters)
-// void consumeNextCode(std::string& morse, int currentIndex, std::vector<char>& letters)
 std::unique_ptr<Node> consumeNextCode(std::string& morse, int currentIndex, int usedLetters)
 {
 	int remainingCharacters = morse.length() - currentIndex;
@@ -49,88 +41,54 @@ std::unique_ptr<Node> consumeNextCode(std::string& morse, int currentIndex, int 
 		// Get the next sequence that matches the code length
 		std::string currentSequence = morse.substr(currentIndex, codeLength);
 
-		for (auto iter = lettersToCode.begin(); iter != lettersToCode.end(); ++iter)
+		// Check to see if the sequence exists in the map
+		if (codeToLetters.count(currentSequence) == 0) {
+			continue;
+		}
+
+		// Make sure we haven't used this letter before
+		char currentLetter = codeToLetters[currentSequence];
+
+		int bitMask = 1 << (25 - (currentLetter - 'a'));
+		if ((usedLetters & bitMask) != 0)
 		{
-			// We want to match the longest codes first
-			if (iter->second.length() != codeLength)
-			{
-				continue;
-			}
+			continue;
+		}
 
-			// Attempt to match the current sequence with a letter
-			if (currentSequence != iter->second)
-			{
-				continue;
-			}
+		// If we did find a match, we can create a new node with the letter.
+		std::unique_ptr<Node> newNode = std::make_unique<Node>(currentLetter);
 
-			// Make sure we haven't used this letter before
-			int bitMask = 1 << (25 - (iter->first - 'a'));
-			if ((usedLetters & bitMask) != 0)
-			{
-				continue;
-			}
-			/*
-			bool letterUsed = false;
-			for (auto charIter = letters.begin(); charIter != letters.end(); ++charIter)
-			{
-				if (iter->first == *charIter)
-				{
-					letterUsed = true;
-					break;
-				}
-			}
+		// Add the letter to the list of used letters
+		usedLetters ^= bitMask;
 
-			if (letterUsed)
-			{
-				continue;
-			}
-			*/
+		// We need to determine if this node is termianl.
+		// If it is, we just want to return it.
+		if (currentIndex + codeLength == morse.length())
+		{
+			newNode->setTerminal(true);
+			return std::move(newNode);
+		}
 
-			// If we did find a match, we can create a new node with the letter.
-			std::unique_ptr<Node> newNode = std::make_unique<Node>(iter->first);
+		// Try to grab the next letter in the sequence
+		std::unique_ptr<Node> nextNode = consumeNextCode(morse, currentIndex + codeLength, usedLetters);
 
-			// Add the letter to the list of used letters
+		// We need to check if we were given an error node. If we were, we need to look for more matches.
+		if (nextNode->getLetter() == 0)
+		{
 			usedLetters ^= bitMask;
-			// letters.push_back(iter->first);
+			continue;
+		}
 
-			// We need to determine if this node is termianl.
-			// If it is, we just want to return it.
-			if (currentIndex + codeLength == morse.length())
-			{
-				newNode->setTerminal(true);
-				return std::move(newNode);
-				//return;
-			}
-
-			std::unique_ptr<Node> nextNode = consumeNextCode(morse, currentIndex + codeLength, usedLetters);
-			// std::unique_ptr<Node> nextNode = consumeNextCode(morse, currentIndex + codeLength, letters);
-			// consumeNextCode(morse, currentIndex + codeLength, letters);
-
-			// We need to check if we were given an error node. If we were, we need to look for more matches.
-			// if (letters.back() == 0)
-			if (nextNode->getLetter() == 0)
-			{
-				usedLetters ^= bitMask;
-				// Pop the error
-				// letters.pop_back();
-				// Pop the letter that was tried
-				// letters.pop_back();
-				continue;
-			}
-
-			// If we didn't get an error node, this means that we can attach the newNode to the current node and return the current node.
-			else
-			{
-				newNode->setNext(std::move(nextNode));
-				return std::move(newNode);
-				// return;
-			}
+		// If we didn't get an error node, this means that we can attach the newNode to the current node and return the current node.
+		else
+		{
+			newNode->setNext(std::move(nextNode));
+			return std::move(newNode);
 		}
 	}
 
 	// We didn't find any matches, return an error node
 	return std::make_unique<ErrorNode>();
-	// letters.push_back(0);
 }
 
 int main() {
@@ -153,34 +111,20 @@ int main() {
 		}
 		std::cout << std::endl;
 	}
-	/*
-	std::vector<char> permutation;
 
-	consumeNextCode(morse, 0, permutation);
-
-	for (char letter : permutation)
-	{
-		std::cout << letter;
-	}
-	std::cout << std::endl;
-	*/
 	// Bonus
 	std::cout << "Bonus" << std::endl;
-	// std::vector<std::vector<char>> outputs;
-	std::vector<std::unique_ptr<Node>> outputs;
 	std::vector<std::future<std::unique_ptr<Node>>> futures;
-
+	std::vector<std::unique_ptr<Node>> outputs;
 
 	std::ifstream inFileStream("smorse2_bonus.in");
 	std::string line;
 
 	auto start = std::chrono::high_resolution_clock::now();
+
 	while (std::getline(inFileStream, line))
 	{
-		int usedLetters = 0;
 		futures.push_back(std::async(consumeNextCode, line, 0, 0));
-		// outputs.push_back(consumeNextCode(line, 0, usedLetters));
-		// outputs.push_back(letters);
 	}
 
 	// Get the futures
@@ -206,6 +150,8 @@ int main() {
 	}
 
 	std::cout << "Time taken to find all inputs: " << duration.count() << " milliseconds" << std::endl;
+
+	std::cin.get();
 
 	return 0;
 }
