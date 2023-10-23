4a3c1c7641c85bfda3a26ea6dfd7066c37aa080b
Ryan Trujillo <54294672+15rtrujillo@users.noreply.github.com>
2023-10-23 11:23:34 -0600

    Added credits/challenge description

--- Smooshed Morse Code 2/main.cpp
+++ Smooshed Morse Code 2/main.cpp
@@ -1,3 +1,22 @@
+/*
+A permutation of the alphabet is a 26-character string in which each of the letters a through z appears once.
+
+Given a smooshed Morse code encoding of a permutation of the alphabet, find the permutation it encodes, or any other permutation that produces the same encoding (in general there will be more than one). It's not enough to write a program that will eventually finish after a very long period of time: run your code through to completion for at least one example.
+
+Examples
+smalpha(".--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..")
+    => "wirnbfzehatqlojpgcvusyxkmd"
+smalpha(".----...---.-....--.-........-----....--.-..-.-..--.--...--..-.---.--..-.-...--..-")
+    => "wzjlepdsvothqfxkbgrmyicuna"
+smalpha("..-...-..-....--.---.---.---..-..--....-.....-..-.--.-.-.--.-..--.--..--.----..-..")
+    => "uvfsqmjazxthbidyrkcwegponl"
+Again, there's more than one valid output for these inputs.
+
+Optional bonus 1
+Here's a list of 1000 inputs. How fast can you find the output for all of them? A good time depends on your language of choice and setup, so there's no specific time to aim for.
+https://www.reddit.com/r/dailyprogrammer/comments/cn6gz5/20190807_challenge_380_intermediate_smooshed/
+*/
+
 #define MULTITHREADING true
 #define LONGEST_CODE 4
 
